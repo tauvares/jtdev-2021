57168940ae7a71284610adfaf5165ec6439e5c4d
João Tavares Filho Segundo <tauvares@gmail.com>
2023-11-22 04:04:20 -0500

    Atualizar pacotes e CVs

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     'jsx-a11y/click-events-have-key-events': 0,
     'jsx-a11y/no-static-element-interactions': 0,
     'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/control-has-associated-label': 0
   },
 };
